1e372d08614a7ff996f5a958821f2dca2710539e
Borodun <a.borodin2@g.nsu.ru>
2020-12-21 14:14:49 +0700

    reworked game

--- Lab3/snake/obstacleMap.h
+++ Lab3/snake/obstacleMap.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "global_parameters.h"
+#include "../src/globalParameters.h"
 
 class BigSquareObstacle {
 private:
--- Lab3/src/main.cpp
+++ Lab3/src/main.cpp
@@ -8,7 +8,7 @@ int main() {
     for (int i = 0; i < playerCount; ++i) {
         types[i] = bot;
     }
-    //types[0] = human;
+    types[0] = human;
 
     Controller controller(playerCount, types);
 
